e51447f0a6325f79a64f59568a1bf3caa8443bdf
balwant <balwantlit@gmail.com>
2020-08-01 01:32:39 +0530

    first commit

---
+++ Mavenjava/src/test/java/SeleniumPractice/AppTest.java
@@ -0,0 +1,19 @@
+package SeleniumPractice;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+    	System.out.println( "Hello World!" );
+    }
+}
---
+++ Mavenjava/src/test/java/SeleniumPractice/SeleniumTest.java
@@ -0,0 +1,15 @@
+package SeleniumPractice;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void browserAutomation() {
+		System.out.println("browserAutomation");
+	}
+	
+	@Test
+	public void elemenstUI() {
+		System.out.println("elemenstUI");
+	}
+}
